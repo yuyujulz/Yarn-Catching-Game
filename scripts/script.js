43ea14b68b96a32fbeab9fdb4726cb23583538e2
yuyujulz <ganina.julia2@gmail.com>
2022-11-24 20:07:48 +0000

    working

--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,48 @@ class catchingObjGame {
     constructor(){
         this.ctx = null;
         this.bg = null;
+        this.player = null;
     }
 
     startGame(){
         const canvas = document.getElementById('canvas');
         this.ctx = canvas.getContext("2d");
 
+        const basket = new Basket()
+
         const background = new Image ();
         background.src = './iron-hack-project/images/shrine.jpg';
 
         background.onload = () => {
             this.bg = background;
+            this.updateCanvas()
         }
 }
 
 updateCanvas() {
-    this.ctx.drawImage(this.bg, 0, 0, )
+    this.ctx.drawImage(this.bg, 0, 0, 744, 700 )
 
 
 }
 
+}
+
+class Basket {
+    constructor(width,height, posX,posY) {
+        this.width = width;
+        this.height = height;
+        this.posX = posX;
+        this.posY = posY;
+        this.img = this.createBasket()
+    }
+
+    createBasket() {
+       // const basket = new Image ()
+        //basket.src = '';
+        //return basket
+        ctx.fillRect(320, 640, 150, 60) 
+        ctx.fillStyle = 'white';      
+    }
 }
 //images//
 //let poop = new Image();
@@ -50,9 +72,9 @@ updateCanvas() {
 
 
 
-window.onload = () => {
-    document.getElementById('start-button').onClick = () => {
+/*window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
         const game = new catchingObjGame()
         game.startGame();
     };
-};
+};*/
